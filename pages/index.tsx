e5c958783003d95e83ed707638b8edd8720f9d68
Trong Duong <princ3od@gmail.com>
2023-02-17 20:29:08 +0700

    chore: update favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from 'react';
 
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 import { Box, Button, Container } from '@chakra-ui/react';
@@ -44,15 +45,30 @@ const Home: NextPage = () => {
   };
 
   return (
-    <Container textAlign="center" maxWidth="2xl" mt="25vh" height="100vh">
-      <Box width="100%" display="flex" justifyContent="center" mb="5">
-        <Logo width={256} height={100} />
-      </Box>
-      <Box as="form" onSubmit={handleSubmit}>
-        <SearchBar onChanged={handleOnChanged} suggestions={suggestions} />
-        <Button mt="5" type="submit">Tìm kiếm</Button>
-      </Box>
-    </Container>
+    <>
+      <Head>
+        <title>Timgiuptui | Home</title>
+        <meta name="description" content="Search Vietnamese news" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <Container textAlign="center" maxWidth="2xl" mt="25vh" height="100vh">
+        <Box width="100%" display="flex" justifyContent="center" mb="5">
+          <Logo width={256} height={100} />
+        </Box>
+        <Box as="form" onSubmit={handleSubmit}>
+          <SearchBar onChanged={handleOnChanged} suggestions={suggestions} />
+          <Button mt="5" type="submit">
+            Tìm kiếm
+          </Button>
+        </Box>
+      </Container>
+    </>
   );
 };
 
--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState } from 'react';
 
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 import { Box, Button, Container, Flex, Select, Skeleton, SlideFade } from '@chakra-ui/react';
@@ -194,104 +195,117 @@ const Search: NextPage = () => {
   const noResult = searchParmas.q !== '' && articles.length === 0;
 
   return (
-    <Container maxWidth="4xl">
-      <Logo />
-      <div className="search-container">
-        <Skeleton isLoaded={routerReady} w="100%" height="48px" borderRadius="8px" my="5">
-          <SearchBar onChanged={onQueryChanged} initialQuery={searchParmas.q} disabled={!routerReady} />
-        </Skeleton>
-        <Button ml="28px" isLoading={isLoading}>
-          Tìm kiếm
-        </Button>
-      </div>
+    <>
+      <Head>
+        <title>Timgiuptui | Search</title>
+        <meta name="description" content="Search Vietnamese news" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <Container maxWidth="4xl">
+        <Logo />
+        <div className="search-container">
+          <Skeleton isLoaded={routerReady} w="100%" height="48px" borderRadius="8px" my="5">
+            <SearchBar onChanged={onQueryChanged} initialQuery={searchParmas.q} disabled={!routerReady} />
+          </Skeleton>
+          <Button ml="28px" isLoading={isLoading}>
+            Tìm kiếm
+          </Button>
+        </div>
 
-      <Flex justifyContent="space-between">
-        <Box display="flex" gap="4" flexGrow="1" mr="8">
-          <Box flex="0.5">
-            <MultiSelect
-              placeholder="Chủ đề"
-              isDisabled={allTopics.length === 0}
-              value={
-                searchParmas.topics &&
-                searchParmas.topics?.map((topic) => ({
-                  value: topic,
-                  label: allTopics.find((t) => t.id === topic)?.name,
-                }))
-              }
-              options={allTopics.map((topic) => ({ value: topic.id, label: topic.name }))}
-              onChange={(topics) => onFilterTopicChanged(topics)}
-              isMulti
-              useBasicStyles
-            />
+        <Flex justifyContent="space-between">
+          <Box display="flex" gap="4" flexGrow="1" mr="8">
+            <Box flex="0.5">
+              <MultiSelect
+                placeholder="Chủ đề"
+                isDisabled={allTopics.length === 0}
+                value={
+                  searchParmas.topics &&
+                  searchParmas.topics?.map((topic) => ({
+                    value: topic,
+                    label: allTopics.find((t) => t.id === topic)?.name,
+                  }))
+                }
+                options={allTopics.map((topic) => ({ value: topic.id, label: topic.name }))}
+                onChange={(topics) => onFilterTopicChanged(topics)}
+                isMulti
+                useBasicStyles
+              />
+            </Box>
+            <Box flex="0.5">
+              <MultiSelect
+                placeholder="Nguồn báo"
+                isDisabled={allSouces.length === 0}
+                value={
+                  searchParmas.sources &&
+                  searchParmas.sources?.map((source) => ({
+                    value: source,
+                    label: allSouces.find((s) => s.id === source)?.name,
+                  }))
+                }
+                options={allSouces.map((source) => ({ value: source.id, label: source.name }))}
+                onChange={(sources) => onFilterSourceChanged(sources)}
+                isMulti
+                useBasicStyles
+              />
+            </Box>
           </Box>
-          <Box flex="0.5">
-            <MultiSelect
-              placeholder="Nguồn báo"
-              isDisabled={allSouces.length === 0}
-              value={
-                searchParmas.sources &&
-                searchParmas.sources?.map((source) => ({
-                  value: source,
-                  label: allSouces.find((s) => s.id === source)?.name,
-                }))
-              }
-              options={allSouces.map((source) => ({ value: source.id, label: source.name }))}
-              onChange={(sources) => onFilterSourceChanged(sources)}
-              isMulti
-              useBasicStyles
-            />
-          </Box>
-        </Box>
 
-        <Box>
-          <Select
-            value={sortBy}
-            disabled={articles.length === 0}
-            onChange={(e) => onSortByChanged(e.target.value as 'relevance' | 'newest' | 'oldest')}
-          >
-            {SORT_BY_TYPES.map(({ value, name }) => (
-              <option key={value} value={value}>
-                {name}
-              </option>
-            ))}
-          </Select>
-        </Box>
-      </Flex>
-      <div className="search-content">
-        {articles.map((article, index) => (
-          <SlideFade
-            in={showResult && articles.findIndex((a) => a.id === article.id) !== -1}
-            unmountOnExit
-            offsetY="20px"
-            key={`${article.id}`}
-            transition={{ enter: { duration: 0.2, delay: (index % 10) * 0.18 } }}
-          >
-            <ArticleCard query={searchParmas.q} article={article} key={`${article.id}`} />
-          </SlideFade>
-        ))}
-        <SlideFade unmountOnExit in={isLoading || noResult} offsetY="20px">
-          {isLoading ? (
-            [...Array(2)].map((_, index) => <ArticleLoading key={index} />)
-          ) : (
-            <SlideFade unmountOnExit in={noResult} offsetY="20px">
-              <NoResult />
+          <Box>
+            <Select
+              value={sortBy}
+              disabled={articles.length === 0}
+              onChange={(e) => onSortByChanged(e.target.value as 'relevance' | 'newest' | 'oldest')}
+            >
+              {SORT_BY_TYPES.map(({ value, name }) => (
+                <option key={value} value={value}>
+                  {name}
+                </option>
+              ))}
+            </Select>
+          </Box>
+        </Flex>
+        <div className="search-content">
+          {articles.map((article, index) => (
+            <SlideFade
+              in={showResult && articles.findIndex((a) => a.id === article.id) !== -1}
+              unmountOnExit
+              offsetY="20px"
+              key={`${article.id}`}
+              transition={{ enter: { duration: 0.2, delay: (index % 10) * 0.18 } }}
+            >
+              <ArticleCard query={searchParmas.q} article={article} key={`${article.id}`} />
             </SlideFade>
-          )}
-        </SlideFade>
-        <Box textAlign="center">
-          <SlideFade unmountOnExit in={showResult} offsetY="20px">
-            {hasMore ? (
-              <Button mt="6" onClick={loadMore} isLoading={isLoadingMore}>
-                Xem thêm
-              </Button>
+          ))}
+          <SlideFade unmountOnExit in={isLoading || noResult} offsetY="20px">
+            {isLoading ? (
+              [...Array(2)].map((_, index) => <ArticleLoading key={index} />)
             ) : (
-              <NoMoreResult />
+              <SlideFade unmountOnExit in={noResult} offsetY="20px">
+                <NoResult />
+              </SlideFade>
             )}
           </SlideFade>
-        </Box>
-        {showResult && <ScrollToTopButton />}
-      </div>
-    </Container>
+          <Box textAlign="center">
+            <SlideFade unmountOnExit in={showResult} offsetY="20px">
+              {hasMore ? (
+                <Button mt="6" onClick={loadMore} isLoading={isLoadingMore}>
+                  Xem thêm
+                </Button>
+              ) : (
+                <NoMoreResult />
+              )}
+            </SlideFade>
+          </Box>
+          {showResult && <ScrollToTopButton />}
+        </div>
+      </Container>
+    </>
   );
 };
 
